90eea6f2ec8343c25bd40ac6dd118500398e12b9
Rishikesh <kesm3052021@gmail.com>
2025-07-25 15:39:38 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
-        brand: "#38b6ff"
-      }
-    }
+        brand: "#38b6ff", // Your NGO brand color
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
